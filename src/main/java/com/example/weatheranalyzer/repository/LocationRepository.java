d6a2d04ee02e104c759b9e223f71f4bc43a91b44
Dmitry Dulko <dimadulko007@gmail.com>
2023-11-28 18:17:37 +0300

    update repository

--- src/main/java/com/example/weatheranalyzer/repository/LocationRepository.java
+++ src/main/java/com/example/weatheranalyzer/repository/LocationRepository.java
@@ -2,6 +2,8 @@ package com.example.weatheranalyzer.repository;
 
 import com.example.weatheranalyzer.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 }
--- src/main/java/com/example/weatheranalyzer/repository/WeatherRepository.java
+++ src/main/java/com/example/weatheranalyzer/repository/WeatherRepository.java
@@ -2,6 +2,9 @@ package com.example.weatheranalyzer.repository;
 
 import com.example.weatheranalyzer.model.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+    Weather findFirstByOrderByIdDesc();
 }
